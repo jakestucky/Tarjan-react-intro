697ae40748d03d8d8c4455a95daa7ce2d4895e4e
Jake Stucky <jakestucky@gmnail.com>
2020-09-15 10:56:23 -0500

    things to learn componenet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import PictureGallery from './PictureGallery';
+import ThingsToLearn from './ThingsToLearn';
 //react component incantation
 class App extends Component {
   render() {
@@ -10,13 +11,10 @@ class App extends Component {
         <h1>Happy react day</h1>
 
         <h3>Things to learn about react:</h3>
-        <ul>
-          <li>HTML side of JS files</li>
-          <li>Class app extends Component</li>
-          <li>Render()?</li>
-          <li>Import/Export?</li>
-        </ul>
+        <ThingsToLearn />
         <h3> Check out my sweet pics</h3>
+        <PictureGallery />
+        <h3> heres another one</h3>
         <PictureGallery />
       </div>
     );
---
+++ src/ThingsToLearn.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class ThingsToLearn extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>HTML side of JS files</li>
+          <li>Class app extends Component</li>
+          <li>Render()?</li>
+          <li>Import/Export?</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default ThingsToLearn;
